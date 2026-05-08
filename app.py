6104822d7dee442c6a4d4a4f7e117d19ae5bb3d2
BigBoss-zer0 <thewaads@gmail.com>
2026-05-08 17:37:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 import requests
-from datetime import datetime, timezone, timedelta
-app = Flask(name)
+
+app = Flask(__name__)
+
 VERIFY_TOKEN = "BotSunny"
-MAKE_WEBHOOK_URL = "https://hook.eu1.make.com/j9b62klim7al7n4kxlxzcqnqszezeaw2"  # ← ใส่ URL จาก Make
-THAI_TZ = timezone(timedelta(hours=7))
+MAKE_WEBHOOK_URL = "https://hook.eu1.make.com/j9b62klim7al7n4kxlxzcqnqszezeaw2"
+
+
 @app.route('/webhook', methods=['GET'])
 def verify():
     token = request.args.get('hub.verify_token')
@@ -12,11 +14,17 @@ def verify():
     if token == VERIFY_TOKEN:
         return challenge
     return 'Invalid token', 403
+
+
 @app.route('/webhook', methods=['POST'])
 def receive_event():
     data = request.json
-    # ส่งต่อไป Make โดยตรง
+    print("=== Received data ===")
+    print(data)
     requests.post(MAKE_WEBHOOK_URL, json=data)
     return 'OK', 200
-if name == 'main':
+
+
+if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
